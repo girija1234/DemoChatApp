02f891add69447e7c59c3b254ebd9ba6b8d4288c
BIPNESWAR <bipneswar.bestrayinfotech@gmail.com>
2014-11-22 00:11:20 +0530

    for express latest version

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = function(app, express, mongoose){
   var config = this;
 
   // Configuration
+    /*
   app.configure(function(){
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
@@ -22,6 +23,27 @@ module.exports = function(app, express, mongoose){
   app.configure('production', function(){
     app.use(express.errorHandler());
   });
+  */
+    // all environments
+    app.set('title', 'Application Title');
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.set('view options', {
+        layout: false
+    });
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.static(__dirname + '/public'));
+    app.use(app.router);
+    // development only
+    if ('development' == app.get('env')) {
+        app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    }
+
+    // production only
+    if ('production' == app.get('env')) {
+        app.use(express.errorHandler());
+    }
   return config;
 
 };
